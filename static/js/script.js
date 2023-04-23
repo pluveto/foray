a92f3697995cdb6a64b83ad6b89cc7432763a87a
pluveto <i@pluvet.com>
2023-04-23 14:52:16 +0800

    update

--- static/js/script.js
+++ static/js/script.js
@@ -71,3 +71,11 @@
     loadMostViews();
     loadRandomPages();
 })()
+
+window.toggleExpand = function toggleExpand(who) {
+    var next = who.nextElementSibling
+    console.log(next.style.display)
+    next.style.display = next.style.display == 'block' ? 'none' : 'block'
+    next.classList.toggle('show')
+    who.querySelector(".icon").classList.toggle('bx-rotate-90')
+}
